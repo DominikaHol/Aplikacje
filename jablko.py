75335db736962dba98b3f03a404a9c422b81df57
DominikaHol <98097626+DominikaHol@users.noreply.github.com>
2022-05-11 11:34:11 +0200

    Add files via upload

--- jablko.py
+++ jablko.py
@@ -1,1 +1,19 @@
+import random
+import pygame
 
+class Jablko():
+    #konstruktor klasy
+    def __init__(self):
+        self.kolor=(255,255,0)
+        self.applePosition=(20,20)
+        self.randomPosition()
+    def setPosition(self,x,y):
+        self.applePosition=(x,y)
+    def getPosition(self):
+        return self.applePosition
+    def randomPosition(self):
+        xApple=random.randint(0,9)*40+20
+        yApple=random.randint(0,9)*40+20
+        self.setPosition(xApple,yApple)
+    def drawApple(self,OknoGry):
+        pygame.draw.circle(OknoGry,self.kolor,(self.applePosition[0],self.applePosition[1]),20)
